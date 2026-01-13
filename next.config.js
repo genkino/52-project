8a2b57974a774a0653310f056842a2ffeed16155
genkino <genkino@gmail.com>
2026-01-13 20:47:42 +0900

    Fix basePath to always use /52-project and simplify deploy script

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const basePath = isProd ? '/52-project' : ''
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: basePath,
-  assetPrefix: basePath,
+  basePath: '/52-project',
+  assetPrefix: '/52-project',
   trailingSlash: true,
 }
 
